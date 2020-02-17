2820864994cc72f108ff0f005e4bf5b9525e8964
William Taylor <will.taylor@bidlogix.net>
2020-02-17 12:04:49 +0000

    added sounds and converted html to js

---
+++ data.js
@@ -0,0 +1,56 @@
+const data = [
+  {
+    "key": "A",
+    "code": "65",
+    "description": "Kick",
+    "src": "sounds/kick.wav"
+  },
+  {
+    "key": "S",
+    "code": "83",
+    "description": "Snare",
+    "src": "sounds/snare.wav"
+  },
+  {
+    "key": "D",
+    "code": "68",
+    "description": "Hi-hat",
+    "src": "sounds/hihat.wav"
+  },
+  {
+    "key": "F",
+    "code": "70",
+    "description": "Open-hat",
+    "src": "sounds/openhat.wav"
+  },
+  {
+    "key": "G",
+    "code": "71",
+    "description": "Boom",
+    "src": "sounds/boom.wav"
+  },
+  {
+    "key": "H",
+    "code": "72",
+    "description": "Ride",
+    "src": "sounds/ride.wav"
+  },
+  {
+    "key": "J",
+    "code": "74",
+    "description": "Clap",
+    "src": "sounds/clap.wav"
+  },
+  {
+    "key": "K",
+    "code": "75",
+    "description": "Tom",
+    "src": "sounds/tom.wav"
+  },
+  {
+    "key": "L",
+    "code": "76",
+    "description": "Tink",
+    "src": "sounds/tink.wav"
+  }
+]
